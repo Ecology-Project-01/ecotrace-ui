0d8583160f82c7042ec07b6a3763959eb85ad10d
Tejal Bhalerao <tejal031@gmail.com>
2026-04-21 11:04:07 +0530

    Added 3 new theme color options in frontend

--- constants/config.js
+++ constants/config.js
@@ -1,3 +1,3 @@
-const LOCAL_IP = "10.99.249.188";
+const LOCAL_IP = " 192.168.137.170";
 //export const API_URL = `https://alternatives-fate-unknown-seem.trycloudflare.com`;
- export const API_URL = `http://10.99.249.188:5000`;
+ export const API_URL = `http:// 192.168.137.170:5000`;
